0159996d176b26139e3fc32e1eee399f0039af95
Steimntz M. de Figueiredo <steimntz@gmail.com>
2017-08-21 14:09:50 -0400

    splits Printer into a module

---
+++ colorize/colorize.py
@@ -0,0 +1,10 @@
+import sys
+
+class Printer:
+    
+    def __init__():
+        sys.stdout.flush()
+
+    def printc(hex_color, text):
+        r, g, b = [int(hex_color[i:i+2], 16) for i in range(1, len(hex_color), 2)]
+        print(f"\x1b[38;2;{r};{g};{b}m{text}\x1b\n")
--- status-bar.py
+++ status-bar.py
@@ -1,21 +1,9 @@
 import sys
 import time
-
-class Printer:
-    def __init__(self):
-        sys.stdout.flush()
-
-    def hi(self):
-        print("hi")
-
-    def printc(self, hex_color, text):
-        r, g, b = [int(hex_color[i:i+2], 16) for i in range(1, len(hex_color), 2)]
-        print(f"\x1b[38;2;{r};{g};{b}m{text}\x1b\n")
+from colorize.printer import Printer
 
 while (1 == 1):
     bar = time.strftime("%c")
     p = Printer()
     p.printc("FF0000", bar)
     time.sleep(1)
-
-sys.stdout.flush()
